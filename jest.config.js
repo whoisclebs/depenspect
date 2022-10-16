379e34b23004e22c4430cef96e17f49f58758fef
Clebson <43012757+clebsonf@users.noreply.github.com>
2022-10-16 14:54:18 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  testEnvironment: 'node',
+  coverageProvider: 'babel',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
